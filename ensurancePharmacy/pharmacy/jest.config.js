a544428294d29e029408c3467cf703e6d3c8bc70
NohelEstrada <dnestrada@unis.edu.gt>
2025-09-11 18:10:27 -0600

    coverage

--- ensurancePharmacy/pharmacy/jest.config.js
+++ ensurancePharmacy/pharmacy/jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['js', 'json', 'vue'],
   transform: {
-    '^.+\\.js$': 'babel-jest'
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.vue$': '@vue/vue3-jest'
   },
   moduleNameMapping: {
     '^@/(.*)$': '<rootDir>/src/$1'
@@ -11,10 +12,20 @@ module.exports = {
     '**/tests/**/*.spec.js',
     '**/tests/**/*.test.js'
   ],
-  collectCoverage: false,
+  collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
     '!src/main.js',
+    '!src/components/HelloWorld.vue',
     '!**/node_modules/**'
-  ]
+  ],
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageDirectory: 'coverage',
+  globals: {
+    '@vue/vue3-jest': {
+      compilerOptions: {
+        isCustomElement: (tag) => tag.startsWith('ion-')
+      }
+    }
+  }
 };
